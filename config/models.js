f10c5559d32d39f510986c656ab4bdcdc3e2255d
yoojuaan <yoojuaan@gmail.com>
2019-03-24 16:22:02 -0300

    Se elimino la base de datos con mongoDB.

--- config/models.js
+++ config/models.js
@@ -35,7 +35,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 
-  datastore:'mongodb',
+  //datastore:'mongodb',
   // schema: true,
 
 
